b1a6ab208626d68398de15fdebb1f5db955d4346
Turzhyk <andrewurbro@yahoo.com>
2026-08-03 07:56:06 +0200

    fix: change connection strings

--- src/admin/Login/Login.tsx
+++ src/admin/Login/Login.tsx
@@ -8,12 +8,10 @@ import { getLogin, handleLogout } from "../../Controllers/LoginController";
 export default function Login() {
   const [login, setLogin] = useState<string>("");
   const [password, setPassword] = useState<string>("");
-  const [hasError, setHasError] = useState<boolean>(false);
+  const [errorCode, setErrorCode] = useState<number>(100);
   const nav = useNavigate();
-  
-  
+
   async function handleLogin(e: any) {
-    console.log("try");
     e.preventDefault();
     try {
       const requestBody = { login, password };
@@ -22,19 +20,20 @@ export default function Login() {
         requestBody,
       );
       console.log(response.data);
-      setHasError(false);
+      setErrorCode(100);
       localStorage.setItem("access_token", response.data);
       nav("/");
-      // navigate('/')
-    } catch (error:any) {
-      
-      // console.log(error.status);
-      setHasError(true);
+    } catch (error: any) {
+      if (axios.isAxiosError(error)) {
+        setErrorCode(error.response?.status ?? 0);
+      } else {
+        setErrorCode(0);
+      }
     }
   }
   return (
     <div className="mt-60 w-120 h-full p-10  bg-amber-100">
-        <h1 className="text-center">Logowanie pracownika</h1>
+      <h1 className="text-center">Logowanie pracownika</h1>
       {getLogin() != null && <div>allready logged in </div>}
       <form onSubmit={(e) => handleLogin(e)}>
         <div className={styles.input_field + " mt-5"}>
@@ -56,8 +55,11 @@ export default function Login() {
           ></input>
         </div>
       </form>
-      {hasError && (
+      {errorCode == 401 && (
         <strong className="text-red-800">Invalid login or/and password</strong>
+      )}
+      {errorCode == 400 && (
+        <strong className="text-red-800">Bad request (Most likely the API is acting freaky)</strong>
       )}
 
       <button
